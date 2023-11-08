5f48dbc0f401f8710138a25ea4dc2d67594bdf0c
emilypandinha <136619754+emilypandinha@users.noreply.github.com>
2023-11-08 11:40:32 -0300

    ok

--- main.js
+++ main.js
@@ -3,22 +3,50 @@ function tocaSomAplausos() {
 }
 document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
 
-function tocaSomTambores() {
+
+function tocaSomVaia() {
+    document.querySelector('#som_tecla_vaia').play();
+   }
+   document.querySelector('tecla_vaia').onclick=tocaSomVaia;
+
+
+function tocaSomRisadas() {
+    document.querySelector('#som_tecla_risadas').play();
+   }
+   document.querySelector('tecla_risadas').onclick=tocaSomRisadas;
+
+
+   function tocaSomErrado() {
+    document.querySelector('#som_tecla_errado').play();
+   }
+   document.querySelector('tecla_errado').onclick=tocaSomErrado;
+
+
+   function tocaSomGrilo() {
+    document.querySelector('#som_tecla_grilo').play();
+   }
+   document.querySelector('tecla_grilo').onclick=tocaSomGrilo;
+
+
+   function tocaSomTambores() {
     document.querySelector('#som_tecla_tambores').play();
    }
    document.querySelector('tecla_tambores').onclick=tocaSomTambores;
 
-function tocaSomAplausos() {
-    document.querySelector('#som_tecla_aplausos').play();
-   }
-   document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
 
-   function tocaSomAplausos() {
-    document.querySelector('#som_tecla_aplausos').play();
+   function tocaSomTrombeta() {
+    document.querySelector('#som_tecla_trompeta').play();
    }
-   document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+   document.querySelector('tecla_trompeta').onclick=tocaSomTrombeta;
 
-   function tocaSomAplausos() {
-    document.querySelector('#som_tecla_aplausos').play();
+
+   function tocaSomDinheiro() {
+    document.querySelector('#som_tecla_dinheiro').play();
    }
-   document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+   document.querySelector('tecla_dinheiro').onclick=tocaSomDinheiro;
+   
+   
+   function tocaSomVitoria() {
+    document.querySelector('#som_tecla_vitoria').play();
+   }
+   document.querySelector('tecla_vitoria').onclick=tocaSomVitoria;
