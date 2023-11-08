786bae6c440f74465e04e8a67490cedce2ecd186
emilypandinha <136619754+emilypandinha@users.noreply.github.com>
2023-11-08 11:22:31 -0300

    ok

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ function tocaSomAplausos() {
  document.querySelector('#som_tecla_aplausos').play();
 }
 document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+
+function tocaSomTambores() {
+    document.querySelector('#som_tecla_tambores').play();
+   }
+   document.querySelector('tecla_tambores').onclick=tocaSomTambores;
+
+function tocaSomAplausos() {
+    document.querySelector('#som_tecla_aplausos').play();
+   }
+   document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+
+   function tocaSomAplausos() {
+    document.querySelector('#som_tecla_aplausos').play();
+   }
+   document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+
+   function tocaSomAplausos() {
+    document.querySelector('#som_tecla_aplausos').play();
+   }
+   document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
