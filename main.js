ed2346a37be414841fd6d470a5bdba0ddd24da20
emilypandinha <136619754+emilypandinha@users.noreply.github.com>
2023-11-22 12:08:50 -0300

    ok

--- main.js
+++ main.js
@@ -1,52 +1,52 @@
 function tocaSomAplausos() {
  document.querySelector('#som_tecla_aplausos').play();
 }
-document.querySelector('tecla_aplausos').onclick=tocaSomAplausos;
+document.querySelector('.tecla_aplausos').onclick=tocaSomAplausos;
 
 
 function tocaSomVaia() {
     document.querySelector('#som_tecla_vaia').play();
    }
-   document.querySelector('tecla_vaia').onclick=tocaSomVaia;
+   document.querySelector('.tecla_vaia').onclick=tocaSomVaia;
 
 
 function tocaSomRisadas() {
     document.querySelector('#som_tecla_risadas').play();
    }
-   document.querySelector('tecla_risadas').onclick=tocaSomRisadas;
+   document.querySelector('.tecla_risadas').onclick=tocaSomRisadas;
 
 
    function tocaSomErrado() {
     document.querySelector('#som_tecla_errado').play();
    }
-   document.querySelector('tecla_errado').onclick=tocaSomErrado;
+   document.querySelector('.tecla_errado').onclick=tocaSomErrado;
 
 
    function tocaSomGrilo() {
     document.querySelector('#som_tecla_grilo').play();
    }
-   document.querySelector('tecla_grilo').onclick=tocaSomGrilo;
+   document.querySelector('.tecla_grilo').onclick=tocaSomGrilo;
 
 
    function tocaSomTambores() {
     document.querySelector('#som_tecla_tambores').play();
    }
-   document.querySelector('tecla_tambores').onclick=tocaSomTambores;
+   document.querySelector('.tecla_tambores').onclick=tocaSomTambores;
 
 
    function tocaSomTrombeta() {
     document.querySelector('#som_tecla_trompeta').play();
    }
-   document.querySelector('tecla_trompeta').onclick=tocaSomTrombeta;
+   document.querySelector('.tecla_trompeta').onclick=tocaSomTrombeta;
 
 
    function tocaSomDinheiro() {
     document.querySelector('#som_tecla_dinheiro').play();
    }
-   document.querySelector('tecla_dinheiro').onclick=tocaSomDinheiro;
+   document.querySelector('.tecla_dinheiro').onclick=tocaSomDinheiro;
    
    
    function tocaSomVitoria() {
     document.querySelector('#som_tecla_vitoria').play();
    }
-   document.querySelector('tecla_vitoria').onclick=tocaSomVitoria;
+   document.querySelector('.tecla_vitoria').onclick=tocaSomVitoria;
